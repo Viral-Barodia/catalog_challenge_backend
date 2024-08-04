2db4722572c1fab5dd52c5162299ae94d7cc9401
Viral-Barodia <202001007@daiict.ac.in>
2024-08-04 11:02:30 +0530

    deploying to netlify

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const serverless = require("serverless-http");
 
 const app = express();
 
@@ -21,3 +22,5 @@ app.get("/", async (req, res) => {
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
 })
+
+module.exports.handler = serverless(app);
